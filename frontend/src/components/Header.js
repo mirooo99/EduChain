7ebb2c189f6dc83d1764f2a57b4be489f8c8576f
mirooo99 <miroslav.r.1998@gmail.com>
2026-04-02 19:50:09 +0300

    Change Etherscan link and enhance styling
    Updated the link to the Etherscan address and improved its styling.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -7,8 +7,11 @@ function Header() {
       <p>Децентрализирана система за академични сертификати</p>
       <p>Проект на Мирослав Радуканов, ученик в ТУЕС, 8а клас</p>
       <p>Адрес за справка: 
-    <a href="https://etherscan.io/address/0x7F7D5821234e313F4d7dcf1d4fDf4b550AdC9554" target="_blank" rel="noopener noreferrer" style="font-family: monospace; word-break: break-all;">
-        0x7F7D5821234e313F4d7dcf1d4fDf4b550AdC9554
+    <a href="https://sepolia.etherscan.io/address/0x60E9965A231504191E1Fc3F0a80ae1C36a12f84e" 
+       target="_blank" 
+       rel="noopener noreferrer" 
+       style="font-family: monospace; color: #007bff; text-decoration: none; background-color: #f8f9fa; padding: 2px 5px; border-radius: 4px; border: 1px solid #dee2e6;">
+        0x60E9965A231504191E1Fc3F0a80ae1C36a12f84e
     </a>
 </p>
     </header>
